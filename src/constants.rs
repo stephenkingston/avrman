8c162575000ed164f4d0c45fc3d00348d08eba47
Stephen Kingston <stephen.psk@gmail.com>
2025-05-05 17:23:47 +0100

    Make programming a teensy bit faster

--- src/constants.rs
+++ src/constants.rs
@@ -1,5 +1,5 @@
 pub(crate) const SERIAL_TIMEOUT_MS: u64 = 1;
-pub(crate) const TRANSPORT_THREAD_SLEEP_MICROS: u64 = 1000;
+pub(crate) const TRANSPORT_THREAD_SLEEP_MICROS: u64 = 100;
 pub(crate) const MAX_RESPONSE_SIZE: usize = 1024;
 
 pub(crate) const RESET_DTR_RTS_LOW_MICROS: u64 = 100;
